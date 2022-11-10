f4a7e8a8e028baa3c6633104dcbb818937d35b5d
Avinash Patel <avinashpatel9755@gmail.com>
2022-11-11 01:00:32 +0530

    Adding

---
+++ src/Components/HomePage/HomePage.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Container, Box } from '@chakra-ui/react';
+
+const HomePage = () => {
+    return (
+        <>
+            <Container border={'1px solid red'} w={'100%'} m={[10, "auto", 0, 'auto']} >
+                <Box h={100} border={'1px solid green'}>
+
+                </Box>
+                <Box h={100} border={'1px solid green'}>
+
+                </Box>
+                <Box h={100} border={'1px solid green'}>
+
+                </Box>
+                <Box h={100} border={'1px solid green'}>
+
+                </Box>
+                <Box h={100} border={'1px solid green'}>
+
+                </Box>
+                <Box h={100} border={'1px solid green'}>
+
+                </Box>
+                <Box h={100} border={'1px solid green'}>
+
+                </Box>
+                <Box h={100} border={'1px solid green'}>
+
+                </Box>
+                <Box h={100} border={'1px solid green'}>
+
+                </Box>
+                <Box h={100} border={'1px solid green'}>
+
+                </Box>
+                <Box h={100} border={'1px solid green'}>
+
+                </Box>
+                <Box h={100} border={'1px solid green'}>
+
+                </Box>
+            </Container>
+        </>
+    )
+}
+
+export default HomePage
